abdde000a78bdebb05a96948b29f21e0ffd85291
johandev456 <johanjohanrosariolopez@gmail.com>
2026-08-28 19:23:28 -0400

    feat: logout

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -32,5 +32,13 @@ export const login = async (req,res)=>{
     }
 }
 
+export const logout = async (req,res)=>{
+    res.clearCookie("token", {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "lax",
+    }).status(200).json({ message: "Logout exitoso!" });
+}
+
 
 
