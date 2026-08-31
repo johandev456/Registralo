805cc9a9a4e4a1f01b3de51a2741840659044fba
johandev456 <johanjohanrosariolopez@gmail.com>
2026-08-30 23:18:06 -0400

    fix: spell mistake

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -2,7 +2,7 @@ import { Navigate,Outlet } from "react-router-dom";
 import { useAuthStore } from "../store/authStore";
 
 const ProtectedRoute=()=>{
-    const user= userAuthStore((state)=>state.user)
+    const user= useAuthStore((state)=>state.user)
     return user ? <Outlet /> : <Navigate to="/login" />
 }
 
